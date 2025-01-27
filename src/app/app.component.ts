53ea5e88c01860d1668404f32519e7f3576fa4f3
Touluse <tal.argamanbib@gmail.com>
2025-01-27 13:32:09 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,6 +43,6 @@ export class AppComponent implements OnInit {
 
   getArenasImage(): string {
     const selectedArena = this.arenas.find((s) => s.name === this.arena);
-    return selectedArena ? selectedArena.image : './assets/images/arenas/default-arena-bg3.png'; // Fallback image
+    return selectedArena ? selectedArena.image : './assets/images/arenas/default-arena-bg.png'; // Fallback image
   }
 }
