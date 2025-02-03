c99aceb088afb996d14a4d52954ba5fdc5b92ebb
Touluse <tal.argamanbib@gmail.com>
2025-02-03 21:22:27 +0200

    mobile

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -109,9 +109,9 @@ export class CharacterService {
 
                 // Apply loser effect
                 if (loser === fighter1 && fighter1Element) {
-                    fighter1Element.style.filter = 'grayscale(60%)';
+                    fighter1Element.style.filter = 'grayscale(70%)';
                 } else if (loser === fighter2 && fighter2Element) {
-                    fighter2Element.style.filter = 'grayscale(60%)';
+                    fighter2Element.style.filter = 'grayscale(70%)';
                 }
 
             }, 1850);
